65a5be9abc05a3d9e1f032c1d5a791bc94b2979a
Hugo Josefson <hugo@josefson.org>
2022-08-26 22:51:17 +0200

    feat: port tcp_simulator example

---
+++ deps.ts
@@ -0,0 +1,18 @@
+import EventEmitter from "https://deno.land/std@0.153.0/node/events.ts";
+export { EventEmitter };
+
+export {
+  createServer,
+  Socket,
+} from "https://deno.land/std@0.153.0/node/net.ts";
+
+export {
+  ButtonPopup,
+  ConfirmPopup,
+  ConsoleManager,
+  CustomPopup,
+  FileSelectorPopup,
+  InputPopup,
+  OptionPopup,
+  PageBuilder,
+} from "npm:console-gui-tools";
